6d34f0507b44dfe7f9f9762313548fae53068a66
Peter Bikundo <binmonk@gmail.com>
2025-07-13 11:24:00 +0300

    feat: Add Factory pattern for easy system configuration - BasketFactory provides pre-configured basket instances - Encapsulates default product catalogue setup - Demonstrates Factory pattern for object creation - Makes system easy to use and configure

---
+++ src/Factory/BasketFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\Cart\Factory;
+
+use Acme\Cart\Entities\Product;
+use Acme\Cart\Services\Basket;
+use Acme\Cart\Services\ProductCatalogue;
+use Acme\Cart\Services\TieredDeliveryRule;
+use Acme\Cart\Services\Offers\RedWidgetBuyOneGetSecondHalfPriceOffer;
+use Acme\Cart\ValueObjects\Money;
+use Acme\Cart\ValueObjects\ProductCode;
+
+/**
+ * Factory for creating pre-configured basket instances
+ */
+final class BasketFactory
+{
+    public static function createWithDefaultConfiguration(): Basket
+    {
+        $catalogue = self::createDefaultCatalogue();
+        $deliveryRule = new TieredDeliveryRule();
+        $offers = [
+            new RedWidgetBuyOneGetSecondHalfPriceOffer()
+        ];
+
+        return new Basket($catalogue, $deliveryRule, $offers);
+    }
+
+    private static function createDefaultCatalogue(): ProductCatalogue
+    {
+        $products = [
+            new Product(
+                new ProductCode('R01'),
+                'Red Widget',
+                Money::fromDollars(32.95)
+            ),
+            new Product(
+                new ProductCode('G01'),
+                'Green Widget',
+                Money::fromDollars(24.95)
+            ),
+            new Product(
+                new ProductCode('B01'),
+                'Blue Widget',
+                Money::fromDollars(7.95)
+            ),
+        ];
+
+        return new ProductCatalogue($products);
+    }
+}
